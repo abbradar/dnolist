b9ff582fc1d48a9f5e8c3f34fb001db47ff87585
Nikolay Amiantov <ab@fmap.me>
2015-07-03 01:51:08 +0300

    added nix file

---
+++ default.nix
@@ -0,0 +1,20 @@
+{ mkDerivation, aeson, attoparsec, base, bytestring, conduit-extra
+, either, email, esqueleto, HaskellNet, monad-loops, network
+, persistent, persistent-postgresql, persistent-template
+, pwstore-fast, servant, servant-client, servant-server, smtp
+, stdenv, text, time, transformers, warp, yaml
+}:
+mkDerivation {
+  pname = "dnolist";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  buildDepends = [
+    aeson attoparsec base bytestring conduit-extra either email
+    esqueleto HaskellNet monad-loops network persistent
+    persistent-postgresql persistent-template pwstore-fast servant
+    servant-client servant-server smtp text time transformers warp yaml
+  ];
+  license = stdenv.lib.licenses.bsd3;
+}
